a10c5ded161262d92f5d2f0194196a626b9e1cd4
Howard M. Miller <howard.miller@gsa.gov>
2025-05-01 17:17:35 -0400

    Allow API auth headers optionally

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,11 +5,20 @@ module Api
     private
 
     def authenticate_token
+      # Allow the auth headers to be optional for now.
+      # TODO: make this mandatory before ATO
+      return unless request.headers['HTTP_AUTHORIZATION']
+
       authenticate_or_request_with_http_token do |token, options|
         @user = User.find_by!(email: options[:email])
-        return false unless @user.logingov_admin?
+        auth_token = @user.auth_token if @user
 
-        @user.auth_token.valid_token? token
+        # Always fail if the auth_token is missing
+        should_fail = !auth_token
+        # Still run the validation check even if it's supposed to fail.
+        # This should act as a defense against timing attacks.
+        auth_token ||= AuthToken.new
+        auth_token.valid_token?(token) && !should_fail
       end
     end
   end
